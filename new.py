3daae764376fc1fe033be82d2510ae94921925da
Luke Gauntlett <Luke.Gauntlett@Hotmail.co.uk>
2025-08-13 09:45:11 +0100

    logo

---
+++ new.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+
+st.image("../images/Logo.png", width=220)
+
+# centered title
+st.markdown("""<style> h1 {text-align: center;} </style>""", unsafe_allow_html=True)
+
+st.title("BluckBoster")
