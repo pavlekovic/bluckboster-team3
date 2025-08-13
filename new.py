a0b0409ade086f12646af630c81ac2721736993f
Luke Gauntlett <Luke.Gauntlett@Hotmail.co.uk>
2025-08-13 09:52:49 +0100

    drop down

--- new.py
+++ new.py
@@ -1,10 +1,23 @@
 import streamlit as st
 import pandas as pd
+from main import df_loaded
 
 
-st.image("../images/Logo.png", width=220)
+st.image("images/Logo.png", width=220)
 
 # centered title
 st.markdown("""<style> h1 {text-align: center;} </style>""", unsafe_allow_html=True)
 
 st.title("BluckBoster")
+
+
+titles = (
+    df_loaded["title"]
+    .dropna()
+    .astype(str)
+    .drop_duplicates()
+    .sort_values()
+    .tolist()
+)
+
+selected = st.selectbox("Search for movie:", options=titles)
